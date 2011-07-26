0ef4a4b692cf187fa4facb3ea6c6e14c83b0db19
Grunde Waag <grunde.waag@dnv.com>
2011-07-26 12:30:08 +0200

    Added functions for JASApaper implementation.

---
+++ inputMatrix.m
@@ -0,0 +1,15 @@
+function M = inputMatrix(rho, w, k_z)
+% Function for calculating the input matrix for a fluid solid interface.
+% 
+% M = inputMatrix(rho, w, k_z)
+%
+% J. AcoustS. oc.A m.8 9 (4), Pt. 1, April 1991
+% A new efficient algorithm to compute the exact reflection
+% and transmission factors for plane waves in layered absorbing
+% media (liquids and solids)
+% Pierre Cervenka and Pascal Challande
+% Equation 31.
+a = -rho*w^2;
+M = [k_z, -k_z;...
+    a, a];
+end
