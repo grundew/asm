57eaa2524b1377f1428d1d996b3d52104074b2bf
Grunde Waag <grunde.waag@dnv.com>
2011-08-24 16:36:49 +0200

    Cleaned up filthy version of code.

--- Lmatrix.m
+++
@@ -1,9 +0,0 @@
-function L = Lmatrix(alpha, beta, gamma, mu)
-% Function computes the L matrix according to equation 4.3.5 p 99
-% Brekhovskihk
-
-L(1, :) = [1i*xi, 1i*xi, -1i*beta, 1i*beta];
-L(2, :) = [1i*alpha, -1i*alpha, 1i*xi, 1i*xi];
-L(3, :) = [2*mu*xi*gamma, 2*mu*xi*gamma, -2*mu*xi*beta, 2*mu*xi*beta];
-L(4, :) = [-2*mu*xi*alpha, 2*mu*xi*alpha, -2*mu*xi*gamma, -2*mu*xi*gamma];
-end
--- PropMatrix.m
+++
@@ -1,6 +0,0 @@
-function T = PropMatrix(alpha, beta, z)
-% Function propagates the waves with vertical wave number alpha and beta a
-% distance z.
-
-T = diag([exp(1i*alpha*z), exp(-1i*alpha*z), exp(1i*beta*z), exp(-1i*beta*z)]);
-end
