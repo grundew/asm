89806c24664b582a28b56e36b26e05cc28060782
Grunde Waag <grunde.waag@dnv.com>
2011-11-11 11:58:36 +0100

    Added algorithm description.

---
+++ algorithm.m
@@ -0,0 +1,9 @@
+% Algorithm
+%
+% 1. Choose distance from transducers to plate, h1 & h2.
+% 2. Choose q's (k's) and f's.
+% 3. Calculate reflection and transmission coefficients.
+% 4. Calculate plane wave Fourier coefficients of the incoming pressure.
+% 5. Calculate the integral, phase shifting the wave and
+%    multiplying the reflection/transmission coefficient
+% 6. Convolve with the excitation signal.
