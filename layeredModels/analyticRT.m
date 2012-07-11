2bdf05ae6647010997b791cf9c5011497e70f4c3
Grunde Waag <grundew@gmail.com>
2012-07-11 22:01:33 +0200

    Updates for plotting dispersion curves. Added output of factors in R.

--- layeredModels/analyticRT.m
+++ layeredModels/analyticRT.m
@@ -1,4 +1,4 @@
-function [R, T, KK, thetal, thetas, kL, kS, kzL, kzS] = analyticRT(freq, theta, model)
+function [R, T, N1, N2, M1, M2] = analyticRT(freq, theta, model)
 
 % L is half the thickness
 L = 0.5*model.thickness;
@@ -7,16 +7,17 @@ rho_s = model.solid.density;
 c_L = model.solid.v;
 c_S = model.solid.vShear;
 c_f = model.fluid.v;
-  
-T = zeros(length(freq), length(theta));
-R = zeros(length(freq), length(theta));
-KK = zeros(length(freq), length(theta));
-kL = zeros(length(freq), 1);
-kS = zeros(length(freq), 1);
-kzL = zeros(length(freq), length(theta));
-kzS = zeros(length(freq), length(theta));
-thetal = zeros(length(freq), length(theta));
-thetas = zeros(length(freq), length(theta));
+
+% Output parameters
+nt = length(theta);
+nf = length(freq);
+T = zeros(nf, nt);
+R = zeros(nf, nt);
+N1 = zeros(nf, nt);
+N2 = zeros(nf, nt);
+M1 = zeros(nf, nt);
+M2 = zeros(nf, nt);
+
 for i = 1:length(freq);
     % Frequency
     f = freq(i);
@@ -35,21 +36,14 @@ for i = 1:length(freq);
         % Angle shear
         theta_S = asin(K./k);
         
-        % Debug variables
-        KK(i, j) = K;
-        thetal(i, j) = theta_L;
-        thetas(i, j) = theta_S;
-        kzL(i, j) = h*cos(theta_L);
-        kzS(i, j) = k*cos(theta_S);
-        
         a = rho_s*c_L*cos(theta_f)*cos(2*theta_S).^2;
         b = rho_s*c_S*cos(theta_f)*sin(2*theta_S).^2;
-        N1 = a./(rho_f*c_f*cos(theta_L)*sin(2*h*L*cos(theta_L)));
-        N2 = b./(rho_f*c_f*cos(theta_S)*sin(2*k*L*cos(theta_S)));
-        N = N1 + N2;
-        M1 = a./(rho_f*c_f*cos(theta_L)*tan(2*h*L*cos(theta_L)));
-        M2 = b./(rho_f*c_f*cos(theta_S)*tan(2*k*L*cos(theta_S)));
-        M = M1 + M2;
+        N1(i, j) = a./(rho_f*c_f*cos(theta_L)*sin(2*h*L*cos(theta_L)));
+        N2(i, j) = b./(rho_f*c_f*cos(theta_S)*sin(2*k*L*cos(theta_S)));
+        N = N1(i, j) + N2(i, j);
+        M1(i, j) = a./(rho_f*c_f*cos(theta_L)*tan(2*h*L*cos(theta_L)));
+        M2(i, j) = b./(rho_f*c_f*cos(theta_S)*tan(2*k*L*cos(theta_S)));
+        M = M1(i, j) + M2(i, j);
 
         T(i, j) = 2*N./(2*M + 1i*(M.^2 - N.^2 - 1));
         R(i, j) = 1i*(M.^2 - N.^2 + 1)./(2*M + 1i*(M.^2 - N.^2 - 1));
--- scripts/drawDispersion.m
+++ scripts/drawDispersion.m
@@ -1,39 +1,49 @@
+function drawDispersion()
+
+nt = 100;
+thetamax = 0.5;
+theta = linspace(0, thetamax, nt);
+d = 10e-3;
+
+nf = 3000;
+v = 5850;
+vs = v.*linspace(0.45, 0.75, 5);
+fres = v/d/2;
+f =  linspace(0, 2.5*fres, nf);
+
+for vShear = vs
+    % Calculate the reflection coefficient for all f and theta
+    R = getReflectionCoeff(f, theta, v, vShear, d);
+    %% Solid layer immersed in a fluid
+    plotdisp(f/fres, theta, R, v/vShear)
+end
+end
+
+function R = getReflectionCoeff(f, theta, v, vShear, d)
 %% Material parameters
 fluid1 = struct('v', 1500, 'density', 1000);
 fluid3 = fluid1;
-layer = struct('v', 5850, 'density', 7850, 'vShear', 2000);
-nt = 100;
-theta = linspace(0, 1, nt);
-%theta = 1e-4;
-d = 10e-3;
+layer = struct('v', v, 'density', 7850, 'vShear', vShear);
 
-nf = 2000;
-fres = layer.v/2/d;
-f =  linspace(0, 5*fres, nf);
 % Fluid-solid-fluid model
 model = MultiLayerModel(fluid1, layer, fluid3, d);
+% Calculate V
+[R, N1, N2, M1, M2] = analyticRT(f, theta, model); %#ok<ASGLU>
+% Save calculated parameters
+outfn = sprintf('data_%2.2f.mat', v/vShear);
+save(outfn, 'f', 'theta', 'vShear', 'R', 'N1', 'N2', 'M1', 'M2'); 
+end
 
-%% Solid layer immersed in a fluid
-%% Calculate V and W
-V = analyticRT(f, theta, model);
-%% Fluid layer immersed in a fluid
-%% Calculate V and W
-% Rf =  fluidLayerReflectionCoefficient(f, theta, fluid1, layer, fluid1, d);
-
+function plotdisp(x, y, V, clcp)
 %% Plot
-figure
-imagesc(f/fres, theta, abs(V)')
+fig = figure;
+imagesc(x, y, abs(V)')
 axis xy
 xlabel('f/fres')
 ylabel('\theta')
-title(sprintf('Solid layer immersed in fluid. cL/cP = %f', layer.v/layer.vShear))
+title(sprintf('Solid layer immersed in fluid. cL/cS = %2.2f', clcp))
 colormap(gray)
-
-% %% Plot
-% figure
-% imagesc(f/fres, theta, abs(Rf)')
-% axis xy
-% xlabel('f/fres')
-% ylabel('\theta')
-% title('Fluid layer immersed in fluid')
-% colormap(gray)
+outfn = fullfile('/Users/grundew/Dropbox/phd_hive/work/DispersionCurves',...
+    sprintf('curves_%2.2f.fig', clcp));
+saveas(fig, outfn,'fig');
+end
