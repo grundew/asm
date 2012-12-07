8f15d3211482355ec4f87e6096b25396a612816c
Grunde Waag <grunde.waag@dnv.com>
2012-12-07 09:17:04 +0100

    Added function for convolving pulse with single angle plane wave with loss in plate.

--- planeWaveTransmissionTimeSignal.m
+++ planeWaveTransmissionTimeSignal.m
@@ -1,33 +1,37 @@
-function [xR, xT, t, R, T] = planeWaveTimeSignal(model, xPulse, tPulse, theta, dist, nfft)
-% [xR, xT, t] = planeWaveTimeSignal(model, xPulse, tPulse)
+function [x, t, T] = planeWaveTransmissionTimeSignal(model, xPulse, tPulse,...
+    theta, dist, alphaLambda, nfft)
+% [xT, t] = planeWaveTimeSignal(model, xPulse, tPulse, dist, alphaLambda)
 %
-% Calculates the response of a solid plate embedded in a fluid and
-% convolves with an excitation pulse.
+% Calculates the transmission response of a solid plate embedded in a fluid
+% and convolves with an excitation pulse.
 %
 % Input:
 % model - MultiLayerModel object (Defines the material properties).
 % xPulse - The amplitudes of the excitation pulse.
 % tPulse - The time stamps of the excitation pulse samples.
-% theta - Angle of incidence.
 % dist - Distance from target to receiver.
+% alphaLambda - Absorption in the solid plate normalized to the wavelength.
 % nfft - Number of points for the fft
 %
 % Output:
-% xR - Reflected time signal.
 % xT - Transmitted time signal.
 % t - Time.
-% R - Reflection coefficient
 % T - Transmission coefficient
 
 if nargin < 4
     theta = 0;
     dist = 0;
-    nfft = 2^13;
+    alphaLambda = 0;
+    nfft = 2^14;
 elseif nargin < 5
     dist = 0;
-    nfft = 2^13;
+    alphaLambda = 0;
+    nfft = 2^14;
 elseif nargin < 6
-    nfft = 2^13;
+    alphaLambda = 0;
+    nfft = 2^14;
+elseif nargin < 7
+    nfft = 2^14;
 end
 
 fs = 1/(tPulse(2)-tPulse(1));
@@ -37,16 +41,15 @@ f = (0:nfft-1)*fs/nfft;
 Ypulse = ifft(xPulse, nfft);
 
 % Get the reflection and transmission coefficients
-[R, T] = fluidSolidFluidReflectionCoefficient(f, theta, model);
+alphaL = -alphaLambda*f/model.solid.v;
+alphaL(f==0) = alphaL(2)*1e3;
+T = transmissionCoefficientAnalytical(f, sin(theta), model, alphaL);
 
 % Phase shift from propagating distance dist
-kr = 2*pi*f/model.fluid(1).v;
 kt = 2*pi*f/model.fluid(2).v;
-Er = exp(1i*kr*dist);
 Et = exp(1i*kt*dist);
 
 % Convolve the pulse and the reflection/transmission coefficient
-xR = fft(Ypulse(:).*R(:).*Er(:), nfft);
-xT = fft(Ypulse(:).*T(:).*Et(:), nfft);
-t = (0:length(xR)-1)/fs;
+x = fft(Ypulse(:).*T(:).*Et(:), nfft);
+t = (0:length(x)-1)/fs;
 end
