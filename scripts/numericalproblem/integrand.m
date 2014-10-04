f969d331e4cc8f55796ff9b938c2387565fce3b1
Grunde Waag <grunde.waag@hbv.no>
2014-10-04 10:30:00 +0200

    Removed the scripts folders

--- scripts/numericalproblem/integrand.m
+++
@@ -1,9 +0,0 @@
-function I = integrand(q, f, a, x, z, model)
-v_fluid = model.fluid(1).v;
-rho_fluid = model.fluid(1).density;
-kz = 2*pi*f*sqrt(1-q.^2)/v_fluid;
-kx = 2*pi*f*q/v_fluid;
-Phi = planePistonPressureAngularSpectrum(z, kx, kz, a, v_fluid, rho_fluid);
-[~, T] = analyticRTFast(f, asin(q), model);
-I = Phi.*T.*exp(1i*kz*z).*exp(1i*kx.*x);
-end
