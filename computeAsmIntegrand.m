7bf971720e63505360b808fd36c945dfe6e35394
Grunde Waag <grunde.waag@hbv.no>
2014-11-05 13:59:46 +0100

    Made the actual integrand a separate function

---
+++ computeAsmIntegrand.m
@@ -0,0 +1,80 @@
+function [X, theta] = computeAsmIntegrand(func, ntheta, params)
+% [X, theta] = computeAsmIntegrand(func, ntheta, asmParams)
+%
+% 
+% Example:
+% p = generateAsmConfig('water', 'steel');
+% p.f = linspace(100e3, 500e3, 500);
+% ntheta = 500;
+% [X, f] = computeAsmIntegrand(@integrandFluidSolidFluid_planepiston, ntheta, p);
+%
+% figure
+% plot(f, abs(X))
+
+%% Pars default arguments
+if nargin < 3
+    fprintf('Must have at least to input arguments.\n');
+    return;
+end
+
+if ~isa(func, 'function_handle')
+    fprintf('First input must be a function handle.\n');
+    return;
+end
+
+if ~params.debug
+    pdir = pwd();
+    cd(fileparts(mfilename('fullpath')));
+    gitStatus = git('status');
+    
+    isunmodified = ~isempty(regexp(gitStatus, 'modified', 'ONCE'));
+    isnotup2date = ~isempty(regexp(gitStatus, 'up-to-date', 'ONCE'));
+    if  isunmodified || isnotup2date
+        warning('The ASM repos is modified or not up-to-date!')
+    else
+        params.gitInfo = getGitInfo();
+    end
+    cd(pdir)
+end
+
+
+%% Samplings stuff
+f = params.f;
+thetamax = params.thetamax;
+thetamin = 0;
+theta = linspace(thetamin, thetamax, ntheta);
+
+
+%% Unpack parameters
+aRx = params.aRx;
+aTx = params.aTx;
+c_F = params.cf;
+rho_F = params.rho_fluid;
+rho_S = params.rho_solid;
+cp = params.cp;
+cs = params.cs;
+thick = params.thickness;
+d1 = params.distanceTx;
+d3 = params.distanceRx;
+al_dB = params.alphaLambda_dB;
+fres = 0.5*params.cp/params.thickness; %#ok<*NASGU>
+x0 = params.displaceRx;
+refl = params.reflection;
+
+
+%% Check sanity of parameters and give warnings or errors
+% TODO: Implement sanity checks. Throw errors.
+assert(al_dB >=0, 'asm:paramerror', 'The damping must be a positive number.');
+
+
+%% Evaluate integrand over all angles for the point on the axis
+X = zeros(length(f), ntheta);
+for i = 1:length(f)    
+    w = 2*pi*f(i);
+    k = w/c_F;
+
+    X(i, :) = func(theta, f(i), k, d1, d3, aTx, aRx, c_F,...
+        al_dB, refl, rho_F, rho_S, cp, cs, thick, x0);
+end
+
+end
---
+++ integrands/integralFluidSolidFluid_planepiston.m
@@ -0,0 +1,70 @@
+function [X, f] = integralFluidSolidFluid_planepiston(params, varargin)
+% [X, f] = integrandFluidSolidFluid_planepiston(params varargin)
+%
+% Output:
+% X - Computed integral at frequencies, f
+% f - Frequencies
+%
+% References:
+% 1. Orofino, 1992. http://dx.doi.org/10.1121/1.405408
+
+
+%% Samplings stuff
+f = params.f;
+thetamax = params.thetamax;
+thetamin = 0;
+
+%% Unpack parameters
+aRx = params.aRx;
+aTx = params.aTx;
+c_F = params.cf;
+rho_F = params.rho_fluid;
+rho_S = params.rho_solid;
+cp = params.cp;
+cs = params.cs;
+thick = params.thickness;
+d1 = params.distanceTx;
+d3 = params.distanceRx;
+al_dB = params.alphaLambda_dB;
+fres = 0.5*params.cp/params.thickness; %#ok<*NASGU>
+x0 = params.displaceRx;
+refl = params.reflection;
+prgbar = params.progressbar;
+
+
+%% Check sanity of parameters and give warnings or errors
+% TODO: Implement sanity checks. Throw errors.
+assert(al_dB >=0, 'asm:paramerror', 'The damping must be a positive number.');
+
+
+% ASCII Progress bar
+if prgbar
+    nnn  = progress('init', 'Wait');
+    tic       % only if not already called
+    t0 = toc; % or toc if tic has already been called
+    tm = t0;
+end
+
+X = zeros(size(f));
+nf = length(f);
+for i = 1:nf
+    % ASCII Progress bar
+    if prgbar
+        tt = ceil((toc-t0)*(nf-i)/i);
+        nn = datenum(0, 0, 0, 0, 0, tt);
+        nnvec = datevec(nn);
+        prgstr = sprintf('ETA: %.0f HOURS %.0f MIN %.0f SEC',...
+            nnvec(end-2), nnvec(end-1), ceil(nnvec(end)));
+        progress(i/nf, sprintf('%i/%i (%s)', i, nf, prgstr));
+    end
+    
+    w = 2*pi*f(i);
+    k = w/c_F;
+    fun = @(theta_z) integrandPlanePiston(...
+        theta_z, f(i), k, d1, d3, aTx, aRx, c_F,...
+        al_dB, refl, rho_F, rho_S, cp, cs, thick, x0);
+    X(i) = quadgk(fun, thetamin, thetamax, varargin{:});
+    
+end
+
+end
